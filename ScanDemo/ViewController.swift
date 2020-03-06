1d7ca61a51c4d62c7b297542fd5d4af99632f8a8
Fring <zeng1090684394@163.com>
2020-03-06 18:59:01 +0800

    init git

---
+++ ScanDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScanDemo
+//
+//  Created by 曾钊 on 2020/3/6.
+//  Copyright © 2020 曾钊. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
